336138269c0c725f943d0b8f98887ca590681963
Anže Pirc <anze.pirc@dataorbis.com>
2023-06-15 13:23:43 +0200

    commit

--- 89_38/main.go
+++ 89_38/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 )
 
 func main() {
@@ -18,14 +19,24 @@ func main() {
 
 	fmt.Println("-----------------------------------------------------------")
 
-	age := m["James"]
-	fmt.Println("The age of Bond", age)
+	age1 := m["James"]
+	fmt.Println("The age of Bond", age1)
 
-	age = m["Q"]
-	fmt.Println("The age of Q", age)
+	age2 := m["Q"]
+	fmt.Println("The age of Q", age2)
 	//comma ok idiom
 	if v, ok := m["Q"]; !ok {
 		fmt.Println("There is no Q", v)
 	}
 
+	fmt.Println("-----------------------------------------------------------")
+
+	c := 1
+	for i := 0; i < 100; i++ {
+		//statement-statement idiom
+		if x := rand.Intn(5); x == 3 {
+			fmt.Printf("Iteration %v \t Total count %v \t X is %v\n", i, c, x)
+			c++
+		}
+	}
 }
--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 
  # Useful tips\commands
  CTRL+D - označimo in lahko urejamo v več vrsticah hkrati
+ SHIFT+ALT+DOWN - dupliramo vrstic
+ ALT+DOWN - premikamo vrstico
 
  # Zapiski
 Zero based index - se začne z 0 (0-9) --> prvih 10 številk
